b1bbefe8c5b863a39e86624bc3f64785ae1f5ac9
Nathan <tartarus9345@gmail.com>
2019-12-08 21:36:24 +0000

    Added Headers

--- main.py
+++ main.py
@@ -22,21 +22,38 @@ def main(cScreen):
     yshift = 0
     curses.start_color()
     curses.use_default_colors()
+    curses.curs_set(0)
     ScreenDimensions = cScreen.getmaxyx()
     maxx = ScreenDimensions[1]
     maxy = ScreenDimensions[0]
     with open (sys.argv[1],"r") as f:
+        ScreenDimensions = cScreen.getmaxyx()
+        maxx = ScreenDimensions[1]
+        maxy = ScreenDimensions[0]
         data = list(csv.reader(f))
+        headers= True
         while True:
             cScreen.clear()
-            for i in range(0,len(data)) :
-                if i >= maxy :
+            start = 0
+            if headers:
+                start = 1
+            for i in range(start,len(data)) :
+                if i -yshift+start>= maxy :
+                    continue
+                if i < yshift:
                     continue
                 for j in range(xshift,len(data[i])):
-                    if sum(getColumnWidths(data)[xshift:j+1])+j+1 > maxx:
-                        cScreen.addstr(i,maxx-1,">")
+                    if sum(getColumnWidths(data)[xshift:j+1])+j+1 >= maxx:
+                        #cScreen.addstr(i-yshift,maxx-1,">")
                         continue
-                    cScreen.addstr(i,sum(getColumnWidths(data)[xshift:j])+j,data[i+yshift][j])
+                    cScreen.addstr(i-yshift+start,sum(getColumnWidths(data)[xshift:j])+j,data[i][j])
+            if headers:
+                for j in range(xshift,len(data[0])):
+                    if sum(getColumnWidths(data)[xshift:j+1])+j+1 > maxx:
+                        #cScreen.addstr(i,maxx-1,">")
+                        continue
+                    cScreen.addstr(0,sum(getColumnWidths(data)[xshift:j])+j,data[0][j])
+                cScreen.addstr(1,0,"="*maxx)
             cScreen.refresh()
             ch = cScreen.getkey()
             if ch == 'q':
@@ -46,9 +63,11 @@ def main(cScreen):
             if ch == 'h':
                 xshift -=1
             if ch == 'j':
-                yshift -=1
-            if ch == 'k':
                 yshift +=1
+            if ch == 'k':
+                yshift -=1
+            if ch == 'h':
+                headers = (headers == False)
             xshift = clamp(xshift,0,len(data[0]))
             yshift = clamp(yshift,0,len(data)-1)
     pass
